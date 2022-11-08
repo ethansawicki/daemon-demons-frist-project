14b0bbd8f801e9d0940afb35665c127bf034fb13
yogi <ychaudhuri5@gmail.com>
2022-11-08 15:51:07 -0600

    added nav bar

---
+++ src/components/nav/CustomerNav.js
@@ -0,0 +1,39 @@
+import { Link, useNavigate } from "react-router-dom";
+import "./NavBar.css";
+
+export const CustomerNav = () => {
+  const navigate = useNavigate();
+  const localDaemonUser = localStorage.getItem("daemon_user");
+  const daemonUserObject = JSON.parse(localDaemonUser);
+
+  return (
+    <ul className="navbar">
+      <li className="navbar__item active">
+        <Link className="navbar__link" to="/content">
+          Content
+        </Link>
+      </li>
+      <li className="navbar__item active">
+        <Link className="navbar__link" to="/likedContent">
+          Liked Content
+        </Link>
+      </li>
+      {localStorage.getItem("daemon_user") ? (
+        <li className="navbar__item navbar__logout">
+          <Link
+            className="navbar__link"
+            to=""
+            onClick={() => {
+              localStorage.removeItem("daemon_user");
+              navigate("/", { replace: true });
+            }}
+          >
+            Logout
+          </Link>
+        </li>
+      ) : (
+        ""
+      )}
+    </ul>
+  );
+};
