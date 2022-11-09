2bbd3057101fcf160a6926ea9d9fed9746c3bc39
Thomas Kibby <htkibby@gmail.com>
2022-11-08 20:58:45 -0600

    delete added back

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -6,6 +6,24 @@ export const Content = ({ content, users, fetchContent }) => {
    const shorten = (text) => {
       return text?.substring(0, 100)
    }
+   const deleteButton = () => {
+      if (!users.isStaff) {
+         return (
+            <button
+               onClick={() => {
+                  fetch(`http://localhost:8088/content/${content.id}`, {
+                     method: "DELETE",
+                  }).then(fetchContent);
+               }}
+               className="content__delete"
+            >
+               DELETE
+            </button>
+         );
+      } else {
+         return "";
+      }
+   };
 
    return ( <>
       <div className="exhibit">
