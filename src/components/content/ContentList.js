1a6ccbdaf0860c43244e8fcb561faf33a8afa126
ethansawicki <ethansawicki2797@gmail.com>
2022-11-12 12:34:57 -0600

    make like button actually post to db. removed some unused stuff and made like button only show for patrons.

--- src/components/content/ContentList.js
+++ src/components/content/ContentList.js
@@ -4,41 +4,8 @@ import "./content.css"
 
 export const ContentList = () => {
    const [contents, updateContent] = useState([])
-   const [likesWithPatrons, setLiked] = useState([])
-   const [likesOn, toggleLiked] = useState(false)
-
    const localDaemonUser = localStorage.getItem("daemon_user");
    const daemonUserObject = JSON.parse(localDaemonUser);
-
-   useEffect(
-      () => {
-         const fetchLikes = async () => {
-            const response = await fetch(`http://localhost:8088/likes?_expand=patrons&_expand=contents`)
-            const likedArray = await response.json()
-            setLiked(likedArray)
-         }
-         fetchLikes()
-      },
-      []
-   )
-
-   useEffect(
-      () => {
-         if (likesOn) {
-            const filteredLikes = likesWithPatrons.filter(liked => liked.id === daemonUserObject.id)
-            setLiked(filteredLikes)
-         } else {
-            setLiked(contents)
-         }
-      },
-      [contents, likesOn]
-   )
-      const fetchContent = async () => {
-      const response = await fetch(`http://localhost:8088/contents`)
-      const contentArray = await response.json()
-      updateContent(contentArray)
-      }
-      fetchContent()
 
    const fetchContent = async () => {
       const response = await fetch(`http://localhost:8088/contents`)
@@ -46,7 +13,6 @@ export const ContentList = () => {
       updateContent(contentArray)
    } 
   
-
    useEffect(
       () => {
          fetchContent()
@@ -57,9 +23,8 @@ export const ContentList = () => {
    return <>
       <h1>Current Exhibits and Shows</h1>
       <article className="contentList">
-         <button onClick={() => { likesOn ? toggleLiked(false) : toggleLiked(true) }}>Only Liked</button>
          {
-            likesWithPatrons.map(
+            contents.map(
                (content) => {
                   return <Content content={content} key={`content--${content.id}`} users={daemonUserObject} />
                }
